04c9c3311fc2f3e82f8a4837f307bb95350505cc
Trelanor <trelanor@gmail.com>
2018-05-31 15:47:47 +0200

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
-particlesJS.load('particles-js', '../styles/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+
+$(document).ready(function() {
+  particlesJS.load('particles-js', '../particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 });
